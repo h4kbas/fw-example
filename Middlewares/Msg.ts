bfdb721950a81d98ade037dcb28093dfb3ed00ce
Hüseyin Akbaş <h4kbas@gmail.com>
2021-03-24 11:36:33 +0300

    Msg and index

--- Middlewares/Msg.ts
+++ Middlewares/Msg.ts
@@ -2,7 +2,7 @@ import { Request } from "fw/Request";
 import { Context } from "fw/FW";
 import Middleware from "fw/Middleware";
 
-export default class Domain implements Middleware {
+export default class Msg implements Middleware {
   public static Before(R: Request, C: Context, Block: Function){
     C.Msg = "Hello";
     //Block();
